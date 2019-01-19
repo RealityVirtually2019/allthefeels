a608789b49d333bd36c3af9ea4ea0d3841a67d79
metaversedance <matthewh913@gmail.com>
2019-01-19 16:59:32 -0500

    add models

---
+++ Assets/Models/BlendShapeWithSin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlendShapeWithSin : MonoBehaviour {
+    private SkinnedMeshRenderer skinnedMeshRenderer;
+    public List<float> speed;
+    public float scale = 100;
+
+    // Use this for initialization
+    void Start () {
+        skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < speed.Count; i++)
+        {
+            if (speed[i] > 0f)
+            {
+                float val = Mathf.PerlinNoise(Time.time * speed[i], Time.time * speed[i]);
+                Debug.Log("perlin" + val);
+                //Debug.Log("scale" + Time.time * scale[i]);
+                skinnedMeshRenderer.SetBlendShapeWeight(i, val * 10);
+
+            }
+
+        }
+        
+
+    }
+}
